7c30350b726368d988f9474b59c53a88693467c2
Elvanss <duy.elvans12@gmail.com>
2024-04-22 23:53:13 +1000

    Disable Auth

--- src/main/java/com/service/foodorderserviceserver/Auth/AuthController.java
+++ src/main/java/com/service/foodorderserviceserver/Auth/AuthController.java
@@ -1,7 +1,9 @@
 package com.service.foodorderserviceserver.Auth;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1/auth")
+@RequestMapping
 public class AuthController {
 }
